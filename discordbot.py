509df5a93afa76dd51023af69e4a5c7d51811a0c
seiya886s <81159776+seiya886s@users.noreply.github.com>
2021-03-22 22:34:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,44 +7,10 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-
-    if message.content.startswith('!trans'):
-        say = message.content
-        say = say[7:]
-        if say.find('-') == -1:
-            str = say
-            detact = translator.detect(str)
-            befor_lang = detact.lang
-            if befor_lang == 'ja':
-                convert_string = translator.translate(str, src=befor_lang, dest='kr')
-                embed = discord.Embed(title='変換結果', color=0xff0000)
-                embed.add_field(name='Befor', value=str)
-                embed.add_field(name='After', value=convert_string.text, inline=False)
-                await message.channel.send(embed=embed)
-            else:
-                convert_string = translator.translate(str, src=befor_lang, dest='ja')
-                embed = discord.Embed(title='変換結果', color=0xff0000)
-                embed.add_field(name='Befor', value=str)
-                embed.add_field(name='After', value=convert_string.text, inline=False)
-                await message.channel.send(embed=embed)
-        else:
-            trans, str = list(say.split('='))
-            befor_lang, after_lang = list(trans.split('-'))
-            convert_string = translator.translate(str, src=befor_lang, dest=after_lang)
-            embed = discord.Embed(title='変換結果', color=0xff0000)
-            embed.add_field(name='Befor', value=str)
-            embed.add_field(name='After', value=convert_string.text, inline=False)
-            await message.channel.send(embed=embed)
-
-    if message.content.startswith('!detect'):
-        say = message.content
-        s = say[8:]
-        detect = translator.detect(s)
-        m = 'この文字列の言語はたぶん ' + detect.lang + ' です。'
-        await message.channel.send(m)
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 
 @bot.command()
@@ -53,5 +19,3 @@ async def ping(ctx):
 
 
 bot.run(token)
-
-@client.event
