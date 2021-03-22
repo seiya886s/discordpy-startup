18de5aa917a1644c8b4099db70c3da7b4ffc7408
seiya886s <81159776+seiya886s@users.noreply.github.com>
2021-03-22 20:12:23 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+import discord
+from googletrans import Translator
+
+TOKEN = 'ODAzNjUzNTU1MDQ3MTA0NTEy.YBA6sg.0GGB2kz4Isy8SgQ4rRJBUCOD6eM'
+
+client = discord.Client()
+translator = Translator()
+
+@client.event
+async def on_ready():
+    print('--------------')
+    print('ログインしました')
+    print(client.user.name)
+    print(client.user.id)
+    print('--------------')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.content.startswith('!trans'):
+        say = message.content
+        say = say[7:]
+        if say.find('-') == -1:
+            str = say
+            detact = translator.detect(str)
+            befor_lang = detact.lang
+            if befor_lang == 'ja':
+                convert_string = translator.translate(str, src=befor_lang, dest='kr')
+                embed = discord.Embed(title='変換結果', color=0xff0000)
+                embed.add_field(name='Befor', value=str)
+                embed.add_field(name='After', value=convert_string.text, inline=False)
+                await message.channel.send(embed=embed)
+            else:
+                convert_string = translator.translate(str, src=befor_lang, dest='ja')
+                embed = discord.Embed(title='変換結果', color=0xff0000)
+                embed.add_field(name='Befor', value=str)
+                embed.add_field(name='After', value=convert_string.text, inline=False)
+                await message.channel.send(embed=embed)
+        else:
+            trans, str = list(say.split('='))
+            befor_lang, after_lang = list(trans.split('-'))
+            convert_string = translator.translate(str, src=befor_lang, dest=after_lang)
+            embed = discord.Embed(title='変換結果', color=0xff0000)
+            embed.add_field(name='Befor', value=str)
+            embed.add_field(name='After', value=convert_string.text, inline=False)
+            await message.channel.send(embed=embed)
+
+    if message.content.startswith('!detect'):
+        say = message.content
+        s = say[8:]
+        detect = translator.detect(s)
+        m = 'この文字列の言語はたぶん ' + detect.lang + ' です。'
+        await message.channel.send(m)
+
+client.run(TOKEN)
